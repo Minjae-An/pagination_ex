7bac01a767d5b000511097f9d0ef641a360a8745
minjae_an <mj3242@naver.com>
2023-08-14 20:35:47 +0900

    feat : .gitmessage.txt 추가

---
+++ pagination_ex/src/main/java/com/example/pagination_ex/PaginationExApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagination_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationExApplication.class, args);
+	}
+
+}
